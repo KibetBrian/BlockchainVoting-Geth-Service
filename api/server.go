1e8862f25cf32389bec091ca7d81574555711506
KibetBrian <briankibet2040@gmail.com>
2022-06-26 21:15:30 +0300

    refactor

--- api/server.go
+++ api/server.go
@@ -2,8 +2,6 @@ package api
 
 import (
 	"log"
-	"net/http"
-	"github.com/KibetBrian/geth/admin"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,32 +22,4 @@ func (s *Server) Start(port string){
 	if err != nil {
 		log.Fatalf("Server Start Failed. Error: %v", err)
 	}
-}
-func (s *Server) SayHello(c *gin.Context){
-	c.JSON(http.StatusOK, gin.H{"Message":"Hello there!"})
-	return
-}
-
-func (s *Server) GetVoters(c *gin.Context){
-	voters := admin.GetVoters()
-	c.JSON(http.StatusOK, gin.H{"Voters": voters})
-	return
-}
-
-func (s *Server) AddVoter(c *gin.Context){
-
-	var req AddVoterRequest
-	err := c.ShouldBindJSON(&req)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-		return
-	}
-	if req.Admin != electionAdmin{
-		log.Println(req.Admin)
-		log.Println(electionAdmin)
-		c.JSON(http.StatusForbidden, "Only admins are allowed to do this operation")
-		return
-	}
-	tx := admin.AddVoter(req.Address)
-	c.JSON(http.StatusOK,tx)
 }
